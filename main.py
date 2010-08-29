c7d371d4c24507d76bd2194d72e647531832bca1
daniel.saul <daniel.saul@0f8baf65-9ea9-1cae-da77-bbd0bb8202ef>
2010-08-29 05:30:52 +0000

    Finished application logic.

--- main.py
+++ main.py
@@ -1,26 +1,112 @@
 import sys
+import math
 
-class Income():
+class Person():
 	name = ""
-	amount = 0.0
+	income = 0.0
+	due = 0.0
 	
-	def __init__(self,name='bill name',amount=0.0):
+	def __init__(self,name='A Person',income=0.0):
 		self.name = name
-		self.amount = float(amount)
+		self.income = float(income)
 
-
-
+class Calculator():
+	people = []
+	charge = 0.0
+	total_income = 0.0
+	
+	def calculate(self):
+		self.total_income = 0.0
+		
+		for i in self.people:
+			self.total_income += i.income
+		
+		for i in self.people:
+			this_inc = i.income
+			ratio = this_inc/self.total_income
+			due = self.charge*ratio
+			
+			# We always want to round up, there is no proper float rounding in 
+			# python so we times it by 100, then round it, then divide it again.
+			due *= 100
+			due = math.ceil(due)
+			due /= 100
+			
+			i.due = due
 
 def main(argv=None):
-	incomes = [
-		Income("Person 1",500),
-		Income("Person 2",1000),
-		Income("Person 3",1500),
+	people = [
+		Person("Person 1",500),
+		Person("Person 2",1000),
+		Person("Person 3",1500),
 	]
-	print "main"
-	pass
-
-
+	
+	calc = Calculator()
+	calc.people = people
+	calc.charge = 42.95
+	calc.calculate()
+	
+	print "UDE Bill Calculator"
+	print "Total income including all parties: ${0:03.2f}".format(calc.total_income)
+	
+	for p in calc.people:
+		print "Amount due by {0} is ${1:03.2f}".format(p.name,p.due)ue
 
 if __name__ == "__main__":
 	sys.exit(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
