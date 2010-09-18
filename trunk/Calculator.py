6e78a8bd1d76207a9d866eb1ce4a4302e8c14af6
daniel.saul <daniel.saul@0f8baf65-9ea9-1cae-da77-bbd0bb8202ef>
2010-09-18 11:46:20 +0000

    More work on the assistant.

---
+++ trunk/Calculator.py
@@ -0,0 +1,39 @@
+class Person():
+	name = ""
+	income = 0.0
+	due = 0.0
+	
+	
+	def __init__(self,name='A Person',income=0.0):
+		self.name = name
+		self.income = float(income)
+
+class Calculator():
+	people = []
+	charge = 0.0
+	total_income = 0.0
+	due_sum = 0.0
+	due_percent = 0.0
+	
+	def calculate(self):
+		self.total_income = 0.0
+		self.due_sum = 0.0
+		
+		for i in self.people:
+			self.total_income += i.income
+		
+		for i in self.people:
+			this_inc = i.income
+			ratio = this_inc/self.total_income
+			due = self.charge*ratio
+			
+			self.due_percent = (math.ceil((due/this_inc)*10000))/100
+			
+			# We always want to round up, there is no proper float rounding in 
+			# python so we times it by 100, then round it, then divide it again.
+			due *= 100
+			due = math.ceil(due)
+			due /= 100
+			
+			i.due = due
+			self.due_sum += due
--- trunk/main-gui.py
+++ trunk/main-gui.py
@@ -6,45 +6,56 @@ import gtk,gio
 import sys
 from Assistant import Assistant
 from AssistantPage import AssistantPage
+from Calculator import Person,Calculator
 # http://zetcode.com/tutorials/pygtktutorial/advancedwidgets/
 
 class App:
-	assistant = None
+	
 	
 	builder = None
 	window = None
+	bill_entry = None
 	tree_view = None
+	col1 = None
+	col1_rend = None
+	col2 = None
+	col2_rend = None
 	store = None
+	res_tv = None
+	
+	asnt = None
+	asnt_p1 = None
+	asnt_p2 = None
+	asnt_p3 = None
+	people = []
+	
 	
 	def __init__(self):
 		
-		assistant = Assistant()
+		self.asnt = Assistant()
+		self.asnt.connect("switch-page",self.__switch_page_cb)
+		self.asnt.window.connect("destroy", self.destroy)
 		
 		self.builder = gtk.Builder()
 		self.builder.add_from_file("window.glade")
 		
-		page1 = AssistantPage()
-		page1.title_side = "Bill"
-		page1.title_top = "Enter Bill Information:"
-		page1.widget = self.builder.get_object("vboxPage1")
-		assistant.add_page(page1)
+		self.asnt_p1 = AssistantPage()
+		self.asnt_p1.title_side = "Bill"
+		self.asnt_p1.title_top = "Enter Bill Information:"
+		self.asnt_p1.widget = self.builder.get_object("vboxPage1")
+		self.asnt.add_page(self.asnt_p1)
 		
-		page2 = AssistantPage()
-		page2.title_side = "Finances"
-		page2.title_top = "Enter the Party's Financial Information:"
-		page2.widget = self.builder.get_object("vboxPage2")
-		assistant.add_page(page2)
+		self.asnt_p2 = AssistantPage()
+		self.asnt_p2.title_side = "Finances"
+		self.asnt_p2.title_top = "Enter the Party's Financial Information:"
+		self.asnt_p2.widget = self.builder.get_object("vboxPage2")
+		self.asnt.add_page(self.asnt_p2)
 		
-		page3 = AssistantPage()
-		page3.title_side = "Results"
-		page3.title_top = "Results:"
-		page3.widget = self.builder.get_object("scrolledwindowPage3")
-		assistant.add_page(page3)
-		
-		
-		
-		
-		assistant.window.connect("destroy", self.destroy)
+		self.asnt_p3 = AssistantPage()
+		self.asnt_p3.title_side = "Results"
+		self.asnt_p3.title_top = "Results:"
+		self.asnt_p3.widget = self.builder.get_object("scrolledwindowPage3")
+		self.asnt.add_page(self.asnt_p3)
 		
 		
 		
@@ -54,6 +65,11 @@ class App:
 		
 		
 		
+		
+		
+		
+		self.people.append(["Person 1","5"])
+		self.people.append(["Person 2","5"])
 		
 		
 		
@@ -69,39 +85,125 @@ class App:
 		
 		
 		# Create View
+		self.bill_entry = self.builder.get_object("billEntry")
+		#self.bill_entry.modify_base(gtk.STATE_NORMAL,gtk.gdk.color_parse("#FF7F7F"))
+		
+		
+		
 		self.tree_view = self.builder.get_object("peopleList")
 		
-		renderer = gtk.CellRendererText()
-		renderer.set_property("editable",True)
-		column = gtk.TreeViewColumn("Name", renderer, text=0)
-		column.set_sort_column_id(0)
-		self.tree_view.append_column(column)
+		self.col1_rend = gtk.CellRendererText()
+		self.col1_rend.connect("edited",self.col1_edited)
+		self.col1_rend.set_property("editable",True)
+		self.col1 = gtk.TreeViewColumn("Name", self.col1_rend, text=0)
+		self.col1.set_sort_column_id(0)
+		self.tree_view.append_column(self.col1)
 		
-		renderer = gtk.CellRendererText()
-		renderer.set_property("editable",True)
-		column = gtk.TreeViewColumn("Earnings", renderer, text=1)
-		column.set_sort_column_id(1)
-		self.tree_view.append_column(column)
+		self.col2_rend = gtk.CellRendererText()
+		self.col2_rend.connect("edited",self.col2_edited)
+		self.col2_rend.set_property("editable",True)
+		self.col2 = gtk.TreeViewColumn("Earnings", self.col2_rend, text=1)
+		self.col2.set_sort_column_id(1)
+		self.tree_view.append_column(self.col2)
+		
+		self.res_tv = self.builder.get_object("resultTextview")
+		
 		
 		# Create Store
 		self.store = gtk.ListStore(str, str)
-		self.store.append(["Person 1","5"])
-		self.store.append(["Person 2","5"])
-		self.tree_view.set_model(self.store)
-		
-		
+		for person in self.people:
+			self.store.append(person)
 		
 		self.tree_view.set_model(self.store)
-		assistant.show()
+		
+		
+		
+		self.tree_view.set_model(self.store)
+		self.asnt.show()
 	
+	def col1_edited(self,renderer,path,newtext):
+		iter = self.store.get_iter_from_string(path)
+		self.store.set(iter,0,newtext)
+		self.people[int(path)][0] = newtext
 	
-	
+	def col2_edited(self,renderer,path,newtext):
+		iter = self.store.get_iter_from_string(path)
+		self.store.set(iter,1,newtext)
+		self.people[int(path)][1] = newtext
 	
 	def destroy(self, widget, data=None):
 		"""
 		Quit on window close.
 		"""
 		gtk.main_quit()
+	
+	def __switch_page_cb(self,assistant,page):
+		if page == self.asnt_p1:
+			pass
+		elif page == self.asnt_p2:
+			pass
+		elif page == self.asnt_p3:
+			
+			calc_people = []
+			for person in self.people:
+				calc_people.append(Person(person[0],person[1]))
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			buf = self.res_tv.get_buffer()
+			buf.set_text(repr(self.people))
+		
+		print "__change_page_cb",page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 	base = App()
